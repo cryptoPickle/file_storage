0d8fd6614de46e2ea8248bb863484c8aa6c32040
Halil <halilibrahimirmak@gmail.com>
2024-07-09 18:16:12 +0100

    wip

--- storage.go
+++ storage.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
 	"strings"
@@ -19,8 +22,12 @@ type Store struct {
 }
 
 type PathKey struct {
-	Pathname string
-	Original string
+	PathName string
+	FileName string
+}
+
+func (p PathKey) FullPath() string {
+	return fmt.Sprintf("%s/%s", p.PathName, p.FileName)
 }
 
 func NewStore(options ...OptionsFn) *Store {
@@ -34,14 +41,51 @@ func NewStore(options ...OptionsFn) *Store {
 	}
 }
 
+func (s *Store) Delete(key string) error {
+	pathKey := s.PathTransformFunc(key)
+
+	ok := s.Has(key)
+	if ok {
+		return os.RemoveAll(pathKey.FullPath())
+	}
+	defer func() {
+		if ok {
+			log.Printf("deleted [%s] from disk", pathKey.FileName)
+		}
+	}()
+
+	return errors.New("can't delete the file")
+}
+
+func (s *Store) Has(key string) bool {
+	pathKey := s.PathTransformFunc(key)
+
+	_, err := os.Stat(pathKey.FullPath())
+
+	return err == fs.ErrNotExist
+}
+
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+
+	return buf, err
+}
+
 func (s *Store) writeStream(key string, r io.Reader) error {
 	pathKey := s.PathTransformFunc(key)
 
-	if err := os.MkdirAll(pathKey.Pathname, os.ModePerm); err != nil {
+	if err := os.MkdirAll(pathKey.PathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	filePath := pathKey.Filename()
+	filePath := pathKey.FullPath()
 	f, err := os.Create(filePath)
 	if err != nil {
 		return err
@@ -56,6 +100,11 @@ func (s *Store) writeStream(key string, r io.Reader) error {
 
 	log.Printf("written (%d) bytes to disk %s \n", n, filePath)
 	return nil
+}
+
+func (s *Store) readStream(key string) (io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	return os.Open(pathKey.FullPath())
 }
 
 type OptionsFn func(*StoreOpts)
@@ -81,11 +130,7 @@ func DefaultTransformFunc(key string) PathKey {
 	}
 
 	return PathKey{
-		Pathname: strings.Join(paths, "/"),
-		Original: hashStr,
+		PathName: strings.Join(paths, "/"),
+		FileName: hashStr,
 	}
-}
-
-func (p PathKey) Filename() string {
-	return fmt.Sprintf("%s/%s", p.Pathname, p.Original)
 }
--- store_test.go
+++ store_test.go
@@ -12,25 +12,57 @@ func TestPathTransformFunc(t *testing.T) {
 	expectedPathName := "1fc6c/388d4/f5030/0ab78/9cc66/2adf1/b9db4/97b45"
 	expectedOriginalKey := "1fc6c388d4f50300ab789cc662adf1b9db497b45"
 
-	if pathname.Pathname != expectedPathName {
-		t.Errorf("expected %v got %v", expectedPathName, pathname.Pathname)
+	if pathname.PathName != expectedPathName {
+		t.Errorf("expected %v got %v", expectedPathName, pathname.PathName)
 	}
 
-	if pathname.Original != expectedOriginalKey {
-		t.Errorf("expected %v got %v", expectedOriginalKey, pathname.Original)
+	if pathname.FileName != expectedOriginalKey {
+		t.Errorf("expected %v got %v", expectedOriginalKey, pathname.FileName)
 	}
 }
 
-func TestStore(t *testing.T) {
+func RemoveFiles(t *testing.T, path string) {
+	if err := os.RemoveAll(path); err != nil {
+		t.Errorf("can't clean the test %s", err)
+	}
+}
+
+// func TestStore(t *testing.T) {
+// 	s := NewStore()
+// 	key := "somepicture"
+// 	p := DefaultTransformFunc(key)
+// 	defer RemoveFiles(t, p.PathName)
+//
+// 	data := []byte("somedata")
+//
+// 	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+// 		t.Error(err)
+// 	}
+//
+// 	r, err := s.Read(key)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+//
+// 	b, _ := io.ReadAll(r)
+//
+// 	fmt.Print(string(b))
+// 	if string(b) != string(data) {
+// 		t.Errorf("want %s have %s ", data, b)
+// 	}
+// }
+
+func TestDelete(t *testing.T) {
 	s := NewStore()
 	key := "somepicture"
 
-	data := bytes.NewReader([]byte("somedata"))
-	p := DefaultTransformFunc(key)
-	if err := s.writeStream(key, data); err != nil {
+	data := []byte("somedata")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
 	}
-	if err := os.RemoveAll(p.Pathname); err != nil {
-		t.Errorf("can't clean the test %s", err)
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
 	}
 }
